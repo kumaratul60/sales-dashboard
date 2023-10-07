0da9e96e609804f44d41f76fc21777d5db1238db
Atul <atulreso1@gmail.com>
2023-10-07 18:54:23 +0530

    add layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Dashboard from "./Dashboard/Dashboard";
 import ErrorPage from "./Error/ErrorPage";
 import HomePage from "./HomePage/HomePage";
-import Layout from "./Layout/Layout";
+import Layout from "./Layouts/Layout";
 import Navbar from "./Header/Navbar";
 
 export { Dashboard, ErrorPage, HomePage, Layout, Navbar };
